f1d385f2e9bb9236698339e79013b40dac902049
Pierre LAURENT <pierrelaurent@numericable.fr>
2021-01-10 17:14:51 +0100

    09/01/21

--- bombe.js
+++ bombe.js
@@ -53,3 +53,97 @@ function animeLed(){
         ledTour = 0;
     }
 }
+
+var go_couleur = "aucune";
+
+function goClick(){
+    go_couleur = this.className.substring(3);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("go_rouge").length - 1; tour++){
+    document.getElementsByClassName("go_rouge")[tour].addEventListener("click", goClick);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("go_vert").length - 1; tour++){
+    document.getElementsByClassName("go_vert")[tour].addEventListener("click", goClick);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("go_orange").length - 1; tour++){
+    document.getElementsByClassName("go_orange")[tour].addEventListener("click", goClick);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("go_blanc").length - 1; tour++){
+    document.getElementsByClassName("go_blanc")[tour].addEventListener("click", goClick);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("go_noir").length - 1; tour++){
+    document.getElementsByClassName("go_noir")[tour].addEventListener("click", goClick);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("go_bleu").length - 1; tour++){
+    document.getElementsByClassName("go_bleu")[tour].addEventListener("click", goClick);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("go_bleu").length - 1; tour++){
+    document.getElementsByClassName("go_bleu")[tour].addEventListener("click", goClick);
+}
+
+function remettreParDefaut(){
+    var thisSauvClass = this.className;
+    var LengthSauvClass = document.getElementsByClassName(thisSauvClass).length;
+    for(tour=0;tour <= LengthSauvClass - 1; tour++){
+        document.getElementsByClassName(thisSauvClass)[0].removeEventListener("click", remettreParDefaut);
+        document.getElementsByClassName(thisSauvClass)[0].classList.remove(thisSauvClass);
+    }
+}
+
+for(tour=0;tour <= document.getElementsByClassName("rouge").length - 1; tour++){
+    document.getElementsByClassName("rouge")[tour].addEventListener("click", remettreParDefaut);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("vert").length - 1; tour++){
+    document.getElementsByClassName("vert")[tour].addEventListener("click", remettreParDefaut);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("orange").length - 1; tour++){
+    document.getElementsByClassName("orange")[tour].addEventListener("click", remettreParDefaut);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("blanc").length - 1; tour++){
+    document.getElementsByClassName("blanc")[tour].addEventListener("click", remettreParDefaut);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("noir").length - 1; tour++){
+    document.getElementsByClassName("noir")[tour].addEventListener("click", remettreParDefaut);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("bleu").length - 1; tour++){
+    document.getElementsByClassName("bleu")[tour].addEventListener("click", remettreParDefaut);
+}
+
+for(tour=0;tour <= document.getElementsByClassName("bleu").length - 1; tour++){
+    document.getElementsByClassName("bleu")[tour].addEventListener("click", remettreParDefaut);
+}
+
+var SourisAppuyéeValeur
+
+function SourisAppuyée(){
+    SourisAppuyéeValeur = true;
+}
+
+function SourisRelachée(){
+    SourisAppuyéeValeur = false;
+}
+
+document.body.addEventListener("mousedown", SourisAppuyée);
+document.body.addEventListener("mouseup", SourisRelachée);
+
+function appliquerCouleur(){
+
+}
+
+for(tour=0;tour <= document.getElementsByTagName("td").length - 1; tour++){
+    if(document.getElementsByTagName("td")[tour].className == ""){
+        document.getElementsByTagName("td")[tour].className = "noir";
+    }
+}
